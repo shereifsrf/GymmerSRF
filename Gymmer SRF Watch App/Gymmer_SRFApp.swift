d1972ac7c829f77948365b54f761e5ca41e66ef6
shereif <chachushereef@gmail.com>
2022-11-18 23:56:42 +0800

    Initial Commit

---
+++ Gymmer SRF Watch App/Gymmer_SRFApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gymmer_SRFApp.swift
+//  Gymmer SRF Watch App
+//
+//  Created by Mohammed Shereif on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Gymmer_SRF_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
